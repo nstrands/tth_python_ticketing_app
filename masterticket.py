795220df8c12e8585425946d1dd438be0c3054cb
Nilsen Strandskov <nstrands18@gmail.com>
2020-01-18 15:43:40 -0500

    Added more user friendly error handling

--- masterticket.py
+++ masterticket.py
@@ -14,29 +14,39 @@ while tickets_remaining >= 1:
 
 	# Prompt the user by name and ask how many tickets they woule like
 	quantity = input("{}, how many tickets would you like to purchase?  ".format(user_name))
-	quantity = int(quantity)
-	# Calculate the price (number of tickets * price) and assign to a variable
-	purchase_total =  TICKET_PRICE * quantity
+	#quantity = int(quantity)
+	# Expect a ValueError and handle it appropriately.
+	try: 
+		quantity = int(quantity)
+		#Raise ValueError if the request is for more tickets than are available
+		if quantity > tickets_remaining:
+			raise ValueError("There are only {} tickets remaining.".format(tickets_remaining))
+	except ValueError as err:
+		# Include the error text in the output
+		print("Oh no, we ran into an issue. {}. Please try again".format(err))
+	else:
+		# Calculate the price (number of tickets * price) and assign to a variable
+		purchase_total =  TICKET_PRICE * quantity
 
-	# Output the price to the screen
-	print("{}, your total for {} tickets will be {}.".format(user_name, quantity, purchase_total))
+		# Output the price to the screen
+		print("{}, your total for {} tickets will be {}.".format(user_name, quantity, purchase_total))
 
-	# Prompt user if they want to proceed. Y/N
-	user_confirm = input("{}, would you like to proceed with this purchase of {}? Y/N  ".format(user_name, purchase_total))
+		# Prompt user if they want to proceed. Y/N
+		user_confirm = input("{}, would you like to proceed with this purchase of {}? Y/N  ".format(user_name, purchase_total))
 
-	# If they want to proceed
-	if user_confirm.lower() == 'y':
+		# If they want to proceed
+		if user_confirm.lower() == 'y':
 
-		# print out to the screen "SOLD!" to confirm purchase
-		# TODO: Gather credit card info and process it.
-		print("SOLD!! Thank you for your purchase of {}.".format(purchase_total))
-		# decrement the tickets remaining by the number of tickets purchased
-		tickets_remaining -= quantity
+			# print out to the screen "SOLD!" to confirm purchase
+			# TODO: Gather credit card info and process it.
+			print("SOLD!! Thank you for your purchase of {}.".format(purchase_total))
+			# decrement the tickets remaining by the number of tickets purchased
+			tickets_remaining -= quantity
 
-	# Else
-	else: 
-		print("Thanks anyways, {}!!".format(user_name))
-		# Thank them by name
+		# Else
+		else: 
+			print("Thanks anyways, {}!!".format(user_name))
+			# Thank them by name
 
 print("I\'m sorry, the tickets are all sold out. :(")
 
