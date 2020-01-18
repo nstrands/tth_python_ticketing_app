a0fb5f311a9dabc1f0b94fe3e359691e0359e28e
Nilsen Strandskov <nstrands18@gmail.com>
2020-01-18 12:33:39 -0500

    Add request user confirmation after displaying price of purchase before proceeding

--- masterticket.py
+++ masterticket.py
@@ -2,6 +2,9 @@ TICKET_PRICE = 10
 
 tickets_remaining = 100
 
+# Run this code continuously until we run out of tickets.
+
+
 # Output how many tickets are remaining using the tickets_remaining variable
 
 print("There are currently {} tickets remaining.".format(tickets_remaining))
@@ -17,3 +20,23 @@ purchase_total =  TICKET_PRICE * quantity
 
 # Output the price to the screen
 print("{}, your total for {} tickets will be {}.".format(user_name, quantity, purchase_total))
+
+# Prompt user if they want to proceed. Y/N
+user_confirm = input("{}, would you like to proceed with this purchase of {}? Y/N  ".format(user_name, purchase_total))
+
+# If they want to proceed
+if user_confirm.lower() == 'y':
+
+	# print out to the screen "SOLD!" to confirm purchase
+	# TODO: Gather credit card info and process it.
+	print("SOLD!! Thank you for your purchase of {}.".format(purchase_total))
+	# decrement the tickets remaining by the number of tickets purchased
+	tickets_remaining -= quantity
+
+# Else
+else: 
+	print("Thanks anyways, {}!!".format(user_name))
+	# Thank them by name
+
+
+# Notify user that the tickets ar sold out.
